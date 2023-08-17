b0a4c19eeb16476f80e70d612c0a6c67d658f8cd
Addison Crump <addison.crump@cispa.de>
2023-08-17 22:06:06 +0200

    wait for all trials to complete

--- run.sh
+++ run.sh
@@ -4,5 +4,10 @@ docker build . -t pdf_cargo_libafl
 
 for i in {0..7}
 do
-  docker run -t -d pdf_cargo_libafl "/work/pdf/fuzz/fuzz.sh" "cargo_libafl" $i
+  docker run -t --name pdf_cargo_libafl-"$i" -d pdf_cargo_libafl "/work/pdf/fuzz/fuzz.sh" "cargo_libafl" $i
+done
+
+for i in {0..7}
+do
+  docker wait pdf_cargo_libafl-"$i"
 done
