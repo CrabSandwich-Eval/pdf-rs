6ed883f5077d7b9cc8434de94e204be39e74a9c0
Addison Crump <addison.crump@cispa.de>
2023-08-17 22:04:28 +0200

    wait for trial to complete in run.sh

--- run.sh
+++ run.sh
@@ -4,5 +4,11 @@ docker build . -t pdf_libfuzzer
 
 for i in {0..7}
 do
-  docker run -t -d pdf_libfuzzer "/work/pdf/fuzz/fuzz.sh" "libfuzzer" $i
+  docker run -t --name pdf_libfuzzer-"$i" -d pdf_libfuzzer "/work/pdf/fuzz/fuzz.sh" "libfuzzer" $i
 done
+
+for i in {0..7}
+do
+  docker wait pdf_libfuzzer-"$i"
+done
+
